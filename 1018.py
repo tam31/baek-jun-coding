03b1c1b61facfc912a32e84ff279aa1fc3c192c9
tam31 <98944772+tam31@users.noreply.github.com>
2022-02-17 21:42:41 +0900

    Add files via upload

---
+++ 1018.py
@@ -0,0 +1,31 @@
+
+n,m=map(int, input().split())
+
+graph = []
+re=[]
+for i in range(n):
+    graph.append(list(input()))
+
+for a in range(n-7):
+    for b in range(m-7):
+        index1=0
+        index2=0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i + j) % 2 == 0:
+                    if graph[i][j] != 'W':
+                        index1 +=1
+                        
+                    if graph[i][j] != 'B':
+                        index2 +=1
+                        
+                else:
+                    if graph[i][j] != 'B':
+                        index1 +=1
+                       
+                    if graph[i][j] != 'W':
+                        index2 +=1
+                       
+        
+        re.append(min(index1, index2))
+print(min(re))
---
+++ 1436.py
@@ -0,0 +1,10 @@
+num = int(input())
+cnt = 0
+n =666
+while True:
+    if '666' in str(n):
+        cnt +=1
+    if cnt == num:
+        print(n)
+        break
+    n +=1
